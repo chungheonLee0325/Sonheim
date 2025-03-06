483f0f4c15f0e49a856e30d442139230ea90baf4
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-03-06 16:56:01 +0900

    init proj

---
+++ Source/Sonheim/SonheimGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SonheimGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASonheimGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASonheimGameMode();
+};
+
+
+
