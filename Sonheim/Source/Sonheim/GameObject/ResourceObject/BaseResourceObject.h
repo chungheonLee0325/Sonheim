a406d73abbb271d34f8a8013ab375d106f7ad5a1
chungheonLee <fire6376@gmail.com>
2025-03-09 16:12:16 +0900

    feat : Base class 추가 - weapon, resourceObject -> Weapon(Player 사용은 Weapon Interface 상속받아 사용) -> ResourceObject(나무, 돌, 펠지움광석... 플레이어가 상호작용가능한 자원 오브젝트 -> 데미지 입힐시 Item generate 방식으로 구현할 예정)
    Feat : Melee Attack 구현 완료
    Melee Attack Anim Notify State에 의해 driven 되는 Melee Attack Skill 구현 완료 -> 관련 data 추가

---
+++ Sonheim/Source/Sonheim/GameObject/ResourceObject/BaseResourceObject.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseResourceObject.generated.h"
+
+UCLASS()
+class SONHEIM_API ABaseResourceObject : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseResourceObject();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
