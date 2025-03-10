9be403ac4a51fe8b44760d2788f97ee0f0c95069
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-03-10 20:50:39 +0900

    add skill set to datatable

---
+++ Sonheim/Source/Sonheim/AreaObject/Skill/Monster/ProjectileBlade.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sonheim/AreaObject/Skill/Base/BaseSkill.h"
+#include "ProjectileBlade.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONHEIM_API UProjectileBlade : public UBaseSkill
+{
+	GENERATED_BODY()
+};
