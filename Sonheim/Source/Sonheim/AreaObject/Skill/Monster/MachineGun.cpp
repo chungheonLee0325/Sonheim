229281b01596ad6708f3aa14c4add1b2bca2136b
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-03-11 16:03:38 +0900

    feat: Skill MachineGun

--- Sonheim/Source/Sonheim/AreaObject/Skill/Monster/MachineGun.cpp
+++ Sonheim/Source/Sonheim/AreaObject/Skill/Monster/MachineGun.cpp
@@ -2,3 +2,56 @@
 
 
 #include "MachineGun.h"
+
+#include "Kismet/KismetMathLibrary.h"
+#include "Sonheim/AreaObject/Base/AreaObject.h"
+#include "Sonheim/AreaObject/Player/SonheimPlayer.h"
+#include "Sonheim/Utilities/LogMacro.h"
+
+UMachineGun::UMachineGun()
+{}
+
+void UMachineGun::OnCastStart(class AAreaObject* Caster, AAreaObject* Target)
+{
+	Super::OnCastStart(Caster, Target);
+	
+	OnCastFire();
+}
+
+void UMachineGun::OnCastTick(float DeltaTime)
+{
+	Super::OnCastTick(DeltaTime);
+}
+
+void UMachineGun::OnCastFire()
+{
+	Super::OnCastFire();
+
+	GetWorld()->GetTimerManager().SetTimer(FireTimer, this, &UMachineGun::FireGun, 0.1f, true);
+}
+
+void UMachineGun::FireGun()
+{
+	FVector StartPos{m_Caster->GetActorLocation()};
+	FVector EndPos{StartPos + UKismetMathLibrary::RandomUnitVectorInConeInDegrees(m_Caster->GetActorForwardVector(), Spread) * Range};
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(m_Caster);
+	FHitResult HitInfo;
+
+	bool bHit{
+		GetWorld()->LineTraceSingleByChannel(HitInfo, StartPos, EndPos, ECollisionChannel::ECC_GameTraceChannel8, Params)
+	};
+	
+	DrawDebugLine(GetWorld(), StartPos, HitInfo.ImpactPoint, FColor::Emerald, false, 1.f, 0, 1.f);
+
+	if (bHit)
+	{
+		FLog::Log();
+		ASonheimPlayer* Player{Cast<ASonheimPlayer>(HitInfo.GetActor())};
+		if (Player)
+		{
+			FAttackData* AttackData = GetAttackDataByIndex(0);
+			m_Caster->CalcDamage(*AttackData, m_Caster, Player, HitInfo);
+		}
+	}
+}
