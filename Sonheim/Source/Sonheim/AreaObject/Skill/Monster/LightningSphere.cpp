e59da5e31f81ab00b76aa5c87e22e427dc8897ea
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-03-12 20:43:09 +0900

    feat: skill - lightningSphere
    일렉판다 번개 구체 구현

--- Sonheim/Source/Sonheim/AreaObject/Skill/Monster/LightningSphere.cpp
+++ Sonheim/Source/Sonheim/AreaObject/Skill/Monster/LightningSphere.cpp
@@ -2,3 +2,69 @@
 
 
 #include "LightningSphere.h"
+
+#include "Sonheim/AreaObject/Base/AreaObject.h"
+#include "Sonheim/AreaObject/Player/SonheimPlayer.h"
+#include "Sonheim/Element/Derived/ElectricBall.h"
+
+ULightningSphere::ULightningSphere()
+{
+	static ConstructorHelpers::FClassFinder<AElectricBall> ElectricBallClass
+	(TEXT("/Script/Engine.Blueprint'/Game/_BluePrint/Element/ElectricBall/BP_ElectricBall.BP_ElectricBall_C'"));
+	if (ElectricBallClass.Succeeded())
+	{
+		ElectricBallFactory = ElectricBallClass.Class;
+	}
+}
+
+void ULightningSphere::OnCastStart(class AAreaObject* Caster, AAreaObject* Target)
+{
+	Super::OnCastStart(Caster, Target);
+	
+	CurrentTime = 0.f;
+	
+	OnCastFire();
+
+}
+
+void ULightningSphere::OnCastTick(float DeltaTime)
+{
+	Super::OnCastTick(DeltaTime);
+
+	CurrentTime += DeltaTime;
+	if (CurrentTime > DelayTime)
+	{
+		CurrentTime = 0.f;
+		OnCastFire();
+	}
+}
+
+void ULightningSphere::OnCastFire()
+{
+	Super::OnCastFire();
+
+	FireElectricBall();
+}
+
+void ULightningSphere::FireElectricBall()
+{
+	AElectricBall* SpawnedElectricBall{
+		GetWorld()->SpawnActor<AElectricBall>(ElectricBallFactory, m_Caster->GetActorLocation(),
+											   m_Caster->GetActorRotation())
+	};
+
+	// ToDo : Notify에서 Index 주입
+	FAttackData* AttackData = GetAttackDataByIndex(0);
+	// ToDo : TempTarget -> m_Target으로 수정
+	ASonheimPlayer* TempTarget{Cast<ASonheimPlayer>(GetWorld()->GetFirstPlayerController()->GetPawn())};
+
+	m_Target = TempTarget;
+	
+	m_TargetPos = m_Caster->GetActorForwardVector();
+
+	if (SpawnedElectricBall)
+	{
+		SpawnedElectricBall->InitElement(m_Caster, m_Target, m_TargetPos, AttackData);
+	}
+}
+
---
+++ Sonheim/Source/Sonheim/Element/Derived/ElectricBall.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ElectricBall.h"
+
+#include "Components/SphereComponent.h"
+#include "Sonheim/AreaObject/Base/AreaObject.h"
+#include "Sonheim/Utilities/LogMacro.h"
+
+
+// Sets default values
+AElectricBall::AElectricBall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	Root->SetSimulatePhysics(false);
+
+}
+
+// Called when the game starts or when spawned
+void AElectricBall::BeginPlay()
+{
+	Super::BeginPlay();
+	Root->OnComponentBeginOverlap.AddDynamic(this, &AElectricBall::OnBeginOverlap);
+
+}
+
+// Called every frame
+void AElectricBall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetActorLocation(GetActorLocation() + DeltaTime * m_TargetLocation * Speed);
+	
+	if (FVector::Dist(StartPos, GetActorLocation()) > Range)
+	{
+		DestroySelf();
+	}
+}
+
+void AElectricBall::InitElement(AAreaObject* Caster, AAreaObject* Target, const FVector& TargetLocation,
+	FAttackData* AttackData)
+{
+	Super::InitElement(Caster, Target, TargetLocation, AttackData);
+
+	StartPos = m_Caster->GetActorLocation();
+	// m_TargetLocation : unit Vector
+	EndPos = StartPos + m_TargetLocation * Range;
+	
+	if (m_Caster->bShowDebug)
+	{
+		DrawDebugLine(GetWorld(), StartPos, EndPos, FColor::Emerald, false, 1.f, 0, 1.f);
+	}
+}
+
+void AElectricBall::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	FHitResult Hit;
+	if (m_Caster)
+	{
+		m_Caster->CalcDamage(*m_AttackData, m_Caster, OtherActor, Hit);
+	}
+	else
+	{
+		FLog::Log("No m_Caster");
+	}
+	
+	DestroySelf();
+}
+
