e5b77218489b773192561a9cb0267e3ae2d8ca65
chungheonLee <fire6376@gmail.com>
2025-03-20 16:38:37 +0900

    feat : 인벤토리, 장비 시스템 추가 구현 - 툴팁 추가, 아이템 스왑 구현 - 마우스 클릭, 드래그 구현 - 아이템 데이터 추가

--- Sonheim/Source/Sonheim/UI/Widget/Player/Inventory/SlotWidget.h
+++ Sonheim/Source/Sonheim/UI/Widget/Player/Inventory/SlotWidget.h
@@ -5,7 +5,12 @@
 #include "CoreMinimal.h"
 #include "Sonheim/ResourceManager/SonheimGameType.h"
 #include "Sonheim/UI/Widget/CustomWidget.h"
+#include "Blueprint/DragDropOperation.h"
+#include "Sonheim/GameManager/SonheimGameInstance.h"
 #include "SlotWidget.generated.h"
+
+class UToolTipWidget;
+class UDragDropSlotOperation;
 
 /**
  * 
@@ -16,25 +21,21 @@ class SONHEIM_API USlotWidget : public UCustomWidget
 	GENERATED_BODY()
 
 	virtual void NativeConstruct() override;
+	virtual void NativeOnMouseEnter(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
 	
 public:
 	// 슬롯 초기화 함수
 	void Init(int Index);
-	// ToDo: 고민해보자..
-	// 슬롯의 타입을 지정해주기 위한 함수
-	//void SetType(ESlotType Type);
 	// 슬롯을 업데이트해주기 위한 함수
-	void UpdateSlot();
-	void NativePreConstruct();
 	void SetItemData(const FItemData* ItemData, int32 NewQuantity);
 	void ClearSlot();
 
 public:
-	// ToDo: 고민해보자..
-	// 현재 슬롯의 타입
-	//UPROPERTY(VisibleAnywhere, Category = "Slot")
-	//ESlotType SlotType;
-
 	// 슬롯에 지정될 이미지
 	UPROPERTY(VisibleAnywhere, Category = "Slot", meta = (BindWidget = "true"))
 	TObjectPtr<class UImage> IMG_Item;
@@ -61,10 +62,54 @@ public:
 	int ItemID;
 	int Quantity;
 
+	// 슬롯 타입 - 인벤토리인지 장비창인지 구분
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
+	EEquipmentSlotType EquipmentSlotType = EEquipmentSlotType::None;
+	
+	// 툴팁 위젯 클래스
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
+	TSubclassOf<UToolTipWidget> ToolTipWidgetClass;
+	
+	// 드래그 드롭 사용 여부
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Slot")
+	bool bSupportsDragDrop = true;
+	
+	// 아이템 클릭 이벤트
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnItemClicked, USlotWidget*, SlotWidget, bool, bIsRightClick);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnItemClicked OnItemClicked;
+	
+	// 아이템 드래그 시작 이벤트
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnItemDragStarted, USlotWidget*, SlotWidget);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnItemDragStarted OnItemDragStarted;
+	
+	// 아이템 드롭 이벤트
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnItemDropped, USlotWidget*, FromSlot, USlotWidget*, ToSlot);
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnItemDropped OnItemDropped;
+	
+private:
+	UPROPERTY()
+	UToolTipWidget* ToolTipInstance;
+	UPROPERTY()
+	USonheimGameInstance* m_GameInstance;
+
 protected:
 	bool IsEmpty() const;
     
 	// 빈 칸에 적용하기 위한 투명 텍스쳐
 	UPROPERTY(EditAnywhere, Category = "Slot")
 	TObjectPtr<class UTexture2D> DefaultTexture;
+};
+
+// 드래그 드롭 오퍼레이션 클래스
+UCLASS()
+class SONHEIM_API UDragDropSlotOperation : public UDragDropOperation
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY()
+	USlotWidget* DraggedSlotWidget;
 };
