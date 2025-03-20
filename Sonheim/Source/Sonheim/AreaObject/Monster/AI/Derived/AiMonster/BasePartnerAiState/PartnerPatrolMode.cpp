5660a63ee37f6cf3c11974019e574efbdaf38080
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-03-20 14:21:52 +0900

    fix: fsm - parternerpatrolMode
    MovetoPlayer에 CheckMovable 추가

--- Sonheim/Source/Sonheim/AreaObject/Monster/AI/Derived/AiMonster/BasePartnerAiState/PartnerPatrolMode.cpp
+++ Sonheim/Source/Sonheim/AreaObject/Monster/AI/Derived/AiMonster/BasePartnerAiState/PartnerPatrolMode.cpp
@@ -181,12 +181,40 @@ void UPartnerPatrolMode::MoveToPlayer()
 	ASonheimPlayer* Player{Cast<ASonheimPlayer>(GetWorld()->GetFirstPlayerController()->GetPawn())};
 	FVector TargetLocation{Player->GetActorLocation()};
 	FVector Direction{Player->GetActorForwardVector()};
-	FVector DesiredLocation{TargetLocation + Direction * 150.f};
 
-	NavSystem->ProjectPointToNavigation(DesiredLocation, Next);
+	for (int32 i{}; i < 36; ++i)
+	{
+		// Direction 회전
+		const float Yaw{0.f + 10.f * i};
+		FQuat Rotation{FQuat(FRotator(0.f, Yaw, 0.f))};
+		Direction = Rotation.RotateVector(Direction);
+		
+		FVector DesiredLocation{TargetLocation + Direction * 150.f};
 
+		if (!NavSystem->ProjectPointToNavigation(DesiredLocation, Next))
+		{
+			// 갈 곳 없으면
+			return;
+		}
+
+		// 공격이 안되는 곳이면
+		//if (!CheckMoveEnable(TargetLocation, Next.Location))
+		if (!USonheimUtility::CheckMoveEnable(this, m_Owner, m_Owner->GetAggroTarget(), TargetLocation, Next.Location))		
+		{
+			// 그럴 일은 없지만 마지막까지 안되면 그냥
+			if (i == 15)
+			{
+				return;
+			}
+			
+			continue;
+		}
+		
+		break;
+	}
+	
 	m_Owner->GetCharacterMovement()->MaxWalkSpeed = 1000.f;
-
+	
 	EPathFollowingRequestResult::Type Result{m_Owner->AIController->MoveToLocation(Next.Location)};
 
 	if (Result == EPathFollowingRequestResult::Type::RequestSuccessful)
