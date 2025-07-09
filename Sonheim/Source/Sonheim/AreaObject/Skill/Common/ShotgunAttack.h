0f8d6324010dde1d4e3fa579699f45d9fc6190e0
ChungChungLee <chungheon.lee0325@gmail.com>
2025-07-09 12:37:49 +0900

    chore : UI 설정 변경 및 hp / sp bar 위치 변경

---
+++ Sonheim/Source/Sonheim/AreaObject/Skill/Common/ShotgunAttack.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Sonheim/AreaObject/Skill/Base/BaseSkill.h"
+#include "ShotgunAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SONHEIM_API UShotgunAttack : public UBaseSkill
+{
+	GENERATED_BODY()
+};
